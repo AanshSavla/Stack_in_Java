528881bd03544895e24bac343da8632c66643ccc
Aansh Savla <aanshsavla2453@gmail.com>
2023-03-04 15:48:51 +0530

    FindFactorial Added

---
+++ FindFactorial.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+class Fact{
+    int number;
+    int findFact(int n){
+        if(n == 1){
+            return 1;
+        }
+        else{
+            return n*findFact(n-1);
+        }
+    }
+}
+
+class Run{
+    public static void main(String[] args) throws IOException {
+        int number,ans;
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter Number:");
+        number = Integer.parseInt(input.readLine());
+        Fact f = new Fact();
+        ans = f.findFact(number);
+        System.out.println("Factorial = "+ans);
+    }
+}
